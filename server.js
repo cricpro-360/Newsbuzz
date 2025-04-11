eaca1c7114fa7864f9da99b27608f4077d937b4e
cricpro-360 <hindusthannews360@gmail.com>
2025-04-11 10:44:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_URL.split('@')[1],
-  api_key: process.env.CLOUDINARY_URL.split('//')[1].split(':')[0],
-  api_secret: process.env.CLOUDINARY_URL.split(':')[2].split('@')[0]
+  cloud_name: 'Newsbuzz',
+  api_key: '974131963864589',
+  api_secret: 'f5HXawyekgvI2QSECiB7jSHwY2A'
 });
 
 // Multer storage with Cloudinary

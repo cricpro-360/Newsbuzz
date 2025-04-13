9709cd3a6978ab60ae54ccc9c3f4d6ffa62fb017
cricpro-360 <hindusthannews360@gmail.com>
2025-04-13 12:11:10 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   phone: { type: String, required: true, unique: true },
-  name: String,
-  avatar: String
+  username: String,
+  password: String,
+  profilePic: String
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);

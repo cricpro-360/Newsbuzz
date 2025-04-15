f43a8e547fde49aeb4092f6eba408bb19f06d2c6
cricpro-360 <hindusthannews360@gmail.com>
2025-04-15 18:40:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,8 +15,11 @@ mongoose.set('strictQuery', true); app.use(cors()); app.use(bodyParser.json());
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log('MongoDB Connected')) .catch((err) => console.error('MongoDB Connection Error:', err));
 
-const postRoutes = require('./models/Post');
-app.use('/Post', postRoutes);
+const postModels = require('./models/posts');
+app.use('/posts', postModels);
+
+const userModels = require('./models/users');
+app.use('/users', userModels);
 
 // Create or update a user app.post('/user/create-or-update', async (req, res) => { try { const { userId, username, profilePic, bio } = req.body; let user = await User.findById(userId);
 

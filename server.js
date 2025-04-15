3ba36d00c300f41b7e09e040759700d4e1974fa0
cricpro-360 <hindusthannews360@gmail.com>
2025-04-15 19:18:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ mongoose.set('strictQuery', true); app.use(cors()); app.use(bodyParser.json());
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log('MongoDB Connected')) .catch((err) => console.error('MongoDB Connection Error:', err));
 
-const postModels = require('./models/posts');
+const postModels = require('./models/Post');
 
-const userModels = require('./models/users');
+const userModels = require('./models/User');
 
 const authRoutes = require('./authRoutes');
 app.use('/posts', authRoutes);
